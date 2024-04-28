a9919554b429666f99f9e13191002d8806df01c0
Liora Milbaum <liora@lmb.co.il>
2024-04-28 19:11:13 +0300

    config form

--- alexandria/main.py
+++ alexandria/main.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from wtforms import Form, TextAreaField
 
 app = Flask(__name__)
 
-@app.route('/')
+class ConfigForm(Form):
+    config = TextAreaField('Config')
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello World'
+
+    form = ConfigForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        config = form.config.data
+
+        return(redirect(url_for('home')))
+
+    return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
